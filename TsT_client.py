3246455283886de67e3daf9401eb438567d7ded2
Dev_Herdson <devherdson@gmail.com>
2017-05-22 10:05:37 +0900

    예제 클라이언트(수정)

--- TsT_client.py
+++ TsT_client.py
@@ -1,19 +1,11 @@
 import sys, socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("127.0.0.1", 8888))
-s.listen(1)
-print("Start Listen Server")
-con, address = s.accept()
-print(address)
+s.connect(("127.0.0.1", 8888))
 while True:
-    data = con.recv(1024)
-    if not data:
-        break
-    print("Message from Client data : %s" % data.decode())
-    msg = data.decode()
-    if (msg == 'quit') :
-        break
-    elif (msg == 'stop') :
-        break
-    con.send(msg.encode())
+   msg = input()
+   s.sendall(msg.encode())
+   data = s.recv(1024)
+   if not data:
+      break
+   print("Echo from Server Data : %s" % data.decode())
 s.close()
