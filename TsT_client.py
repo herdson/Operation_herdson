b330b301e62ca1a19d08addbb2eb9017781492d4
herdson <devherdson@gmail.com>
2017-05-24 10:23:52 +0900

    Renew

--- TsT_client.py
+++ TsT_client.py
@@ -1,11 +1,16 @@
-import sys, socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 8888))
+import sys,socket
+IP="127.0.0.1"
+Port=8888
+
+print("Client online")
+sckt=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+sead=(IP, Port)
+sckt.connect(sead)
+
 while True:
-   msg = input()
-   s.sendall(msg.encode())
-   data = s.recv(1024)
-   if not data:
-      break
-   print("Echo from Server Data : %s" % data.decode())
-s.close()
+    msg=input()
+    sckt.sendall(msg.encode())
+    data=sckt.recv(1024)
+    print("Data from server: %s" % data.decode())
+sckt.close()
